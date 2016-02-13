acb8fa50365d8f55f878eec11d9a9a6cc906bd82
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-02-13 14:32:04 -0500

    Add babel to unit tests

--- test/unit.js
+++ test/unit.js
@@ -1,19 +1,19 @@
 'use strict';
+require('babel-polyfill');
 const Promise = require('bluebird');
 const expect = require('chai').expect;
 let sailsRequest;
 
 describe('unit tests', () => {
-  before(function () {
+  before(async function () {
     this.timeout(5000);
-    return Promise.promisify(require('sails').load)().then(() => {
-      /* sails.request takes a callback instead of returning a Promise, so Promisify it and use a new `sailsRequest` function,
-      which is exactly the same except that it returns a Promise. */
-      sailsRequest = Promise.promisify(sails.request);
-    });
+    await Promise.promisify(require('sails').load)();
+    /* sails.request takes a callback instead of returning a Promise, so Promisify it and use a new `sailsRequest` function,
+    which is exactly the same except that it returns a Promise. */
+    sailsRequest = Promise.promisify(sails.request);
   });
-  it('returns a 404 error when fetching a nonexistent path', () => {
-    return sailsRequest({method: 'get', url: '/this-path-is-invalid'}).catch(err => {
+  it('returns a 404 error when fetching a nonexistent path', async () => {
+    await sailsRequest({method: 'get', url: '/this-path-is-invalid'}).then(expect.fail, err => {
       expect(err.status).to.equal(404);
     });
   });
