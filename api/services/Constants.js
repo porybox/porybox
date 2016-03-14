6950d87fc946e2b9f5c0009f14bf94c70d1a5e97
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-03-13 22:58:09 -0400

    Added pokemon notes

--- api/services/Constants.js
+++ api/services/Constants.js
@@ -2,6 +2,8 @@ exports.BOX_VISIBILITIES = ['listed', 'unlisted'];
 exports.DEFAULT_BOX_VISIBILITY_SETTING = 'listed';
 exports.POKEMON_VISIBILITIES = ['private', 'public', 'readonly'];
 exports.DEFAULT_POKEMON_VISIBILITY_SETTING = 'readonly';
+exports.POKEMON_NOTE_VISIBILITIES = ['private', 'public'];
+exports.DEFAULT_POKEMON_NOTE_VISIBILITY_SETTING = 'public';
 
 /* This defines the preferences that are allowed to be changed by the user. Having them here is
 * better than simply referencing the UserPreferences model, because that model also contains
@@ -15,6 +17,10 @@ exports.CHANGEABLE_PREFERENCES = {
   defaultPokemonVisibility: {
     enum: exports.POKEMON_VISIBILITIES,
     defaultsTo: exports.DEFAULT_POKEMON_VISIBILITY_SETTING
+  },
+  defaultPokemonNoteVisibility: {
+    enum: exports.POKEMON_NOTE_VISIBILITIES,
+    defaultsTo: exports.DEFAULT_POKEMON_NOTE_VISIBILITY_SETTING
   }
 };
 
