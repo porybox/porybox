50476560bf5c49cef1b877a01d24929bf5bfacba
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-04-02 00:31:53 -0400

    Add support for changing passwords
    Addresses #13, but FCs/IGNs/etc. are still missing

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -75,6 +75,40 @@ module.exports = _.mapValues({
     await req.user.deleteAccount();
     return res.redirect('/');
   },
+  async changePassword (req, res) {
+    const params = req.allParams();
+    Validation.requireParams(params, ['oldPassword', 'newPassword']);
+    const oldPassport = await Passport.findOne({user: req.user.name, protocol: 'local'});
+    const validate = oldPassport.validatePassword.bind(oldPassport);
+    const isValid = await Promise.promisify(validate)(params.oldPassword);
+    if (!isValid) {
+      // If the provided oldPassword is incorrect, stop immediately.
+      return res.forbidden('Incorrect password');
+    }
+    // Otherwise, attempt to create a new Passport for the user with the new password.
+    // This is done *before* deleting the old Passport; otherwise, the user will be locked out of their
+    // account if there's an error creating the new Passport.
+    const newPassport = await Passport.create({
+      protocol: 'local',
+      user: req.user.name,
+      password: params.newPassword,
+      accessToken: require('crypto').randomBytes(48).toString('base64')
+    }).catch(err => {
+      // If an error occurs creating the new Passport, throw it and abort the process.
+      // (Usually this will occur when the user's new passwords is invalid, e.g. too short.)
+      throw err.code === 'E_VALIDATION' ? {statusCode: 400, message: 'Invalid new password'} : err;
+    });
+    // Once the new Passport has been created, delete all of the user's old Passports. This has the effect
+    // of clearing the user's sessions.
+    await Passport.destroy({
+      user: req.user.name,
+      protocol: 'local',
+      password: oldPassport.password,
+      // Omit the newly-created Passport -- this line is necessary because the old/new passwords might be the same.
+      id: {not: newPassport.id}
+    });
+    return res.ok();
+  },
   async checkUsernameAvailable (req, res) {
     const params = req.allParams();
     Validation.requireParams(params, 'name');
