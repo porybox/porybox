2729aeeb6ea2152a35c8b70087fdd533f4172a3a
Alistair McKinlay <alistair@10people.co.uk>
2016-04-12 21:24:07 +0100

    Initial pokemon upload working.

---
+++ client/add/pokemon.ctrl.js
@@ -0,0 +1,30 @@
+/**
+ * Controller for handling the dialog to add a new pokemon
+ * @return {function} A controller that contains 2 test elements
+ */
+module.exports = function ($mdBottomSheet, Upload) {
+
+  this.visibilities = require('../../api/services/Constants.js').POKEMON_VISIBILITIES;
+  this.visibility = this.defaultPokemonVisibility;
+  this.box = this.defaultBox ? this.defaultBox.id : undefined;
+  this.file = {};
+
+  this.cancel = function() {
+    $mdBottomSheet.cancel();
+  };
+
+  this.canAdd = function () {
+    return Boolean(this.visibility && this.file);
+  };
+
+  this.answer = function() {
+    this.file.upload = Upload.upload({
+      url: '/uploadpk6',
+      data: {visibility: this.visibility, pk6: this.file, box: this.box},
+    }).then(function (response) {
+      $mdBottomSheet.hide(response.data);
+    }, function (response) {
+      console.log(response.status + ': ' + response.data);
+    });
+  }
+}
