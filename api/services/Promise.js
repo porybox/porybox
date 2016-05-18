b3db931a908c52899fc17dd049975749a46ba3bf
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-05-18 18:37:59 -0400

    Prevent sails from crashing on startup
    It seems like removing babel-register was actually a bad idea, but we didn't notice it before because we use the mocha --compilers option for tests.

--- api/services/Promise.js
+++ api/services/Promise.js
@@ -1,1 +1,6 @@
-module.exports = require('bluebird');
+'use strict';
+const Promise = require('bluebird');
+if (sails.config.environment === 'development') {
+  Promise.config({longStackTraces: true});
+}
+module.exports = Promise;
