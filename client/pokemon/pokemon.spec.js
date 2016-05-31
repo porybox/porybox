d5c4147d9d9d12e9246177c0df4929553d3205bb
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-05-31 01:25:24 -0400

    Add tests for padded TIDs and shininess

--- client/pokemon/pokemon.spec.js
+++ client/pokemon/pokemon.spec.js
@@ -44,5 +44,35 @@ describe('PokemonCtrl', function() {
       expect(tested.id).to.equal('routeParamId');
       expect(Object.keys(tested.pokemon).length).to.equal(0);
     });
+
+    it("pads a Pokémon's TID correctly", function() {
+      const tested1 = $controller(ctrlTest, {$scope, io, $routeParams}, {pokemon: {tid: 12345}});
+      expect(tested1.paddedTid).to.equal('12345');
+      const tested2 = $controller(ctrlTest, {$scope, io, $routeParams}, {pokemon: {tid: 1234}});
+      expect(tested2.paddedTid).to.equal('01234');
+      const tested3 = $controller(ctrlTest, {$scope, io, $routeParams}, {pokemon: {tid: 123}});
+      expect(tested3.paddedTid).to.equal('00123');
+      const tested4 = $controller(ctrlTest, {$scope, io, $routeParams}, {pokemon: {tid: 12}});
+      expect(tested4.paddedTid).to.equal('00012');
+      const tested5 = $controller(ctrlTest, {$scope, io, $routeParams}, {pokemon: {tid: 1}});
+      expect(tested5.paddedTid).to.equal('00001');
+      const tested6 = $controller(ctrlTest, {$scope, io, $routeParams}, {pokemon: {tid: 0}});
+      expect(tested6.paddedTid).to.equal('00000');
+    });
+
+    it('determines shininess correctly', function() {
+      const tested1 = $controller(ctrlTest, {$scope, io, $routeParams}, {pokemon: {
+        pid: 0,
+        tid: 0,
+        sid: 15}
+      });
+      expect(tested1.isShiny()).to.be.true;
+      const tested2 = $controller(ctrlTest, {$scope, io, $routeParams}, {pokemon: {
+        pid: 0,
+        tid: 0,
+        sid: 16}
+      });
+      expect(tested2.isShiny()).to.be.false;
+    });
   });
 });
