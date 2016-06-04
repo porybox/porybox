77f767d9b76acbf52c2c71b104a5105ff29617f0
NotRaia <NotRaia@users.noreply.github.com>
2016-06-03 18:52:20 -0700

    Pretty cards (#150)
    * Slightly prettier cards and header
    
    * Remove testing style line
    
    * Rebase onto master, add egg icon
    
    * Add box name/description placeholder
    
    * Slightly prettier cards and header
    
    * Slightly prettier cards and header
    
    * Remove testing style line
    
    * Remove testing style line
    
    * More prettifying
    
    * Rebase onto master, add egg icon
    
    * svg icon WIP
    
    * Fix svg colors, italicize HAs
    
    * Fix shiny/KB icons always displaying
    
    * Move error image files
    
    * Display both egg sprite and Pokémon for eggs
    
    * Responsive design
    
    * Font change
    
    * Fix icon placement
    
    * fix height bug
    
    * Slightly prettier cards and header
    
    * Remove testing style line
    
    * Add box name/description placeholder
    
    * More prettifying
    
    * Rebase onto master, add egg icon
    
    * svg icon WIP
    
    * Fix svg colors, italicize HAs
    
    * Fix shiny/KB icons always displaying
    
    * Move error image files
    
    * Display both egg sprite and Pokémon for eggs
    
    * Responsive design
    
    * Font change
    
    * Fix icon placement
    
    * fix height bug
    
    * Fix padding for long names
    
    * Fix test issues

--- client/pokemon/pokemon.ctrl.js
+++ client/pokemon/pokemon.ctrl.js
@@ -16,10 +16,11 @@ module.exports = function($routeParams, $http, $scope) {
 
   this.tsv = (this.data.tid ^ this.data.sid) >>> 4;
   this.esv = ((this.data.pid & 0xffff) ^ (this.data.pid >>> 16)) >>> 4;
-  this.isShiny = this.tsv === this.esv;
+  this.isShiny = this.tsv === this.esv && !this.data.isEgg;
   this.isKB = this.data.otGameId >= 24 && this.data.otGameId <= 29;
 
-  this.iconUrl = (this.isShiny ? 'shiny/' : '') + this.data.dexNo;
+  this.iconUrl = `pokemon/${this.isShiny ? 'shiny' : 'regular'}/${
+    this.data.speciesName && this.data.speciesName.toLowerCase()}`;
 
   this.ballNameUrl = this.data.ballName
     ? this.data.ballName.replace(' ', '-').replace('é', 'e').toLowerCase()
@@ -28,4 +29,5 @@ module.exports = function($routeParams, $http, $scope) {
   this.heldItemUrl = this.data.heldItemName
     ? this.data.heldItemName.replace(' ', '-').replace('é', 'e').toLowerCase()
     : null;
-}
+
+};
