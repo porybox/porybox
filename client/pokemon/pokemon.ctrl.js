99108cd4dc24cf59b31a0f877d5e5e6f97f992fc
NotRaia <NotRaia@users.noreply.github.com>
2016-06-14 12:27:26 -0700

    Initial Main Summary view

--- client/pokemon/pokemon.ctrl.js
+++ client/pokemon/pokemon.ctrl.js
@@ -6,6 +6,9 @@ module.exports = function($routeParams, $scope, io) {
   this.id = $routeParams.pokemonid || this.data.id;
   this.parseProps = () => {
     this.paddedTid = ('00000' + this.data.tid).slice(-5);
+    this.paddedSid = ('00000' + this.data.sid).slice(-5);
+    this.paddedEsv = ('0000' + this.data.esv).slice(-4);
+    this.paddedTsv = ('0000' + this.data.tsv).slice(-4);
     this.ivs = [
       this.data.ivHp,
       this.data.ivAtk,
@@ -14,6 +17,72 @@ module.exports = function($routeParams, $scope, io) {
       this.data.ivSpDef,
       this.data.ivSpe
     ];
+
+    this.stats = {
+      HP: {
+        fullName: "Hit Points",
+        iv: this.data.ivHp,
+        ev: this.data.evHp
+      },
+      Atk: {
+        fullName: "Attack",
+        iv: this.data.ivAtk,
+        ev: this.data.evAtk
+      },
+      Def: {
+        fullName: "Defense",
+        iv: this.data.ivDef,
+        ev: this.data.evDef
+      },
+      SpAtk: {
+        fullName: "Special Attack",
+        iv: this.data.ivSpAtk,
+        ev: this.data.evSpAtk
+      },
+      SpDef: {
+        fullName: "Special Defense",
+        iv: this.data.ivSpDef,
+        ev: this.data.evSpDef
+      },
+      Spe: {
+        fullName: "Speed",
+        iv: this.data.ivSpe,
+        ev: this.data.evSpe
+      }
+    };
+
+    this.moves = [
+      {
+        moveName: this.data.move1Name,
+        Pp: this.data.move1Pp,
+        Ppu: this.data.move1Ppu
+      },
+      {
+        moveName: this.data.move2Name,
+        Pp: this.data.move2Pp,
+        Ppu: this.data.move2Ppu
+      },
+      {
+        moveName: this.data.move3Name,
+        Pp: this.data.move3Pp,
+        Ppu: this.data.move3Ppu
+      },
+      {
+        moveName: this.data.move4Name,
+        Pp: this.data.move4Pp,
+        Ppu: this.data.move4Ppu
+      }
+    ];
+
+    this.eggMoves = [
+      this.data.eggMove1Name,
+      this.data.eggMove2Name,
+      this.data.eggMove3Name,
+      this.data.eggMove4Name
+    ];
+
+
+    this.gameLabel = 'label-' + this.data.otGameName.charAt(0).toLowerCase();
 
     this.isKB = this.data.otGameId >= 24 && this.data.otGameId <= 29;
     this.hasHA = this.data.abilityNum === 4;
