2fecb6965cf0769b478112712abeadea85418317
NotRaia <NotRaia@users.noreply.github.com>
2016-07-05 19:57:54 -0700

    Dialog for editing profile info

--- client/profile/profile.ctrl.js
+++ client/profile/profile.ctrl.js
@@ -1,4 +1,7 @@
-module.exports = function($scope, $routeParams, io) {
+const editCtrl = require('./profile-edit.ctrl.js');
+const angular = require('angular');
+
+module.exports = function($scope, $routeParams, io, $mdMedia, $mdDialog) {
   this.data = this.data || {};
   this.data.name = this.data.name || $routeParams.username;
   this.errorStatusCode = null;
@@ -11,14 +14,29 @@ module.exports = function($scope, $routeParams, io) {
       this.errorStatusCode = err.statusCode;
     }).then(() => $scope.$apply());
   };
-  this.edit = () => {
-    const data = {
-      inGameNames: this.editedIgns.split(','),
-      friendCodes: this.editedFcs.split(','),
-      trainerShinyValues: this.editedTsvs.split(',')
-    };
-    return io.socket.postAsync('/me', data).then(() => {
-      Object.assign(this.data, data);
-    }).catch(console.error.bind(console)).then(() => $scope.$apply());
+
+    this.edit = event => {
+    const useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+    return Promise.resolve($mdDialog.show({
+      locals: {data: this.data},
+      bindToController: true,
+      controller: ['$mdDialog', editCtrl],
+      controllerAs: 'dialog',
+      templateUrl: 'profile/profile-edit.view.html',
+      parent: angular.element(document.body),
+      targetEvent: event,
+      clickOutsideToClose: true,
+      fullscreen: useFullScreen
+    }).then((editedData) => {
+      return io.socket.postAsync('/me', editedData).then(() => {
+        Object.assign(this.data, editedData);
+        $scope.$apply();
+      });
+    })).catch(console.error.bind(console));
   };
 };
--- client/profile/profile.module.js
+++ client/profile/profile.module.js
@@ -4,7 +4,7 @@ const profileController = require('./profile.ctrl');
 ng.module('porybox.profile', ['ngRoute']).config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/user/:username', {
     templateUrl: '/profile/profile.view.html',
-    controller: ['$scope', '$routeParams', 'io', profileController],
+    controller: ['$scope', '$routeParams', 'io', '$mdMedia', '$mdDialog', profileController],
     controllerAs: 'profile'
   });
 }]);
