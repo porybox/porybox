d5d744c16b5c011e4b2898d05197e2e5fabe9f64
Alistair McKinlay <alistair@10people.co.uk>
2016-07-07 20:33:01 +0100

    Add button to add multiple FC/TSV/IGNs

--- client/profile/profile-edit.ctrl.js
+++ client/profile/profile-edit.ctrl.js
@@ -3,19 +3,35 @@
  */
 module.exports = function ($mdDialog) {
 
-  this.inGameNames = this.data.inGameNames;
-  this.friendCodes = this.data.friendCodes;
-  this.trainerShinyValues = this.data.trainerShinyValues;
+  this.friendCodes = this.data.friendCodes ? this.data.friendCodes.slice(0) : [''];
+  this.inGameNames = this.data.inGameNames ? this.data.inGameNames.slice(0) : [''];
+  this.tsvs = this.data.trainerShinyValues ? this.data.trainerShinyValues.slice(0) : [''];
 
-  this.cancel = function() {
+  if (this.friendCodes.length === 0) {
+    this.friendCodes = [''];
+  }
+  if (this.inGameNames.length === 0) {
+    this.inGameNames = [''];
+  }
+  if (this.tsvs.length === 0) {
+    this.tsvs = [''];
+  }
+
+  this.addFC = () => this.friendCodes.push('');
+  this.addIGN = () => this.inGameNames.push('');
+  this.addTSV = () => this.tsvs.push('');
+
+  this.cancel = () => {
     $mdDialog.cancel();
   };
 
-  this.answer = function() {
+  this.answer = () => {
     $mdDialog.hide({
-      inGameNames: `${this.inGameNames}`.split(','),
-      friendCodes: `${this.friendCodes}`.split(','),
-      trainerShinyValues: `${this.trainerShinyValues}`.split(',')
+      inGameNames: this.inGameNames.filter(removeEmpty),
+      friendCodes: this.friendCodes.filter(removeEmpty),
+      trainerShinyValues: this.tsvs.filter(removeEmpty)
     });
   };
+
+  const removeEmpty = (el) => el !== '';
 };
