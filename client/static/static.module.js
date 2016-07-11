488cc15104682474ef0286ffc57c04c821355560
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-07-11 00:58:39 -0400

    Move static pages to angular

---
+++ client/static/static.module.js
@@ -0,0 +1,17 @@
+const ng = require('angular');
+ng.module('porybox.static', ['ngRoute']).config(['$routeProvider', $routeProvider => {
+  [
+    'about',
+    'donate',
+    'extracting-pk6-files',
+    'faq',
+    'how-to-pk6-1-bvs',
+    'how-to-pk6-2-homebrew',
+    'how-to-pk6-3-4-save-files',
+    'how-to-pk6-6-decrypted-powersaves',
+    'privacy-policy',
+    'tos'
+  ].forEach(pageName => {
+    $routeProvider.when(`/${pageName}`, {templateUrl: `/static/${pageName}.html`});
+  });
+}]);
