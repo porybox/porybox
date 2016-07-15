0abbf620cf172ad5187e64c960ca745158993020
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-07-14 20:39:45 -0400

    Use prototype functions in add/pokemon.ctrl.js

--- client/add/pokemon.ctrl.js
+++ client/add/pokemon.ctrl.js
@@ -5,22 +5,28 @@ import Promise from 'bluebird';
  * Controller for handling the dialog to add a new pokemon
  * @return {function} A controller that contains 2 test elements
  */
-module.exports = function ($mdDialog, $routeParams) {
-  this.visibilities = require('../../api/services/Constants.js').POKEMON_VISIBILITIES;
-  this.defaultBox = undefined;
 
-  if (this.boxes.length) {
-    this.defaultBox =
-      $routeParams.boxid && this.boxes.map(box => box.id).includes($routeParams.boxid)
-        ? $routeParams.boxid
-        : maxBy(this.boxes, box => +new Date(box.updatedAt)).id;
+module.exports = class PokemonAdd {
+  constructor ($mdDialog, $routeParams) {
+    this.$mdDialog = $mdDialog;
+    this.$routeParams = $routeParams;
+
+    this.visibilities = require('../../api/services/Constants').POKEMON_VISIBILITIES;
+    this.defaultBox = undefined;
+    if (this.boxes.length) {
+      this.defaultBox =
+        this.$routeParams.boxid && this.boxes.map(box => box.id).includes(this.$routeParams.boxid)
+          ? this.$routeParams.boxid
+          : maxBy(this.boxes, box => +new Date(box.updatedAt)).id;
+    }
+    this.lines = [];
   }
 
-  this.lines = [];
+  addLine (line) {
+    this.lines.push(line);
+  }
 
-  this.addLine = (line) => this.lines.push(line);
-
-  this.addFiles = (files) => {
+  addFiles (files) {
     files.forEach(file => this.addLine({
       filename: file.name,
       data: fileToBase64(file),
@@ -29,19 +35,18 @@ module.exports = function ($mdDialog, $routeParams) {
     }));
     this.draggedFiles = [];
     this.manualFiles = [];
-  };
+  }
 
-  this.cancel = function() {
-    return $mdDialog.cancel();
-  };
+  cancel () {
+    return this.$mdDialog.cancel();
+  }
 
-  this.canAdd = function () {
+  canAdd () {
     return this.lines.every((line) => line.visibility && line.data && line.box);
-  };
-
-  this.answer = function() {
-    return $mdDialog.hide(this.lines);
-  };
+  }
+  answer () {
+    return this.$mdDialog.hide(this.lines);
+  }
 };
 
 function fileToBase64 (file) {
