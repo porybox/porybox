0c6b3b4cfe86533e1c8acb053dd5e652a4e95eed
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-07-17 16:26:14 -0400

    Fix template strings and make the email service more flexible

--- api/services/email.js
+++ api/services/email.js
@@ -2,21 +2,13 @@ const nodemailer = require('nodemailer');
 
 const user = sails.config.email ? sails.config.email.user : '';
 const pass = sails.config.email ? sails.config.email.user : '';
-const transporter = nodemailer.createTransport(`smtp://@{user}:@{pass}@smtp.elasticemail.com:2525`);
+const transporter = nodemailer.createTransport(`smtp://${user}:${pass}@smtp.elasticemail.com:2525`);
 
-const mailOptions = {
-  from: '"Porybox" <noreply@porybox.com>',
-  to: '',
-  subject: 'Hello ✔',
-  html: '<b>Hello world</b>'
-};
-
-exports.sendMail = (address) => {
-  mailOptions.to = address;
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error){
-      return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
+exports.sendMail = ({from, to, subject, body}) => {
+  return Promise.promisify(transporter.sendMail.bind(transporter))({
+    from, to, subject, html: body
+  }).tap(result => {
+    sails.log.verbose(`Sent an email to ${to} with subject '${subject}'`);
+    sails.log.verbose(result);
   });
 };
