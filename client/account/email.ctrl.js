2063229d1a85533bd4ab7c05672d6024196b1c36
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-07-31 21:12:42 -0400

    Add a clientside form for updating email addresses

---
+++ client/account/email.ctrl.js
@@ -0,0 +1,28 @@
+module.exports = class Email {
+  constructor ($scope, io, $mdToast, errorHandler) {
+    this.$scope = $scope;
+    this.io = io;
+    this.$mdToast = $mdToast;
+    this.errorHandler = errorHandler;
+  }
+  editEmail () {
+    return this.io.socket.postAsync('/api/v1/changeEmail', {
+      email: this.newEmail,
+      password: this.password
+    }).then(() => {
+      this.reset();
+      return this.$mdToast.show(this.$mdToast.simple()
+        .position('bottom right')
+        .textContent('Email address updated successfully'));
+    }).catch({statusCode: 401, body: 'Incorrect password'}, () => {
+      this.$scope.editEmailForm.password.$setValidity('correct', false);
+      this.$scope.$apply();
+    }).catch(this.errorHandler);
+  }
+  reset () {
+    this.newEmail = '';
+    this.password = '';
+    this.$scope.editEmailForm.$setPristine();
+    this.$scope.editEmailForm.$setUntouched();
+  }
+};
