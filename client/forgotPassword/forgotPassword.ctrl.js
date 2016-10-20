9460086125b6830f5c40207b4bad78ffd6b71214
Alistair McKinlay <github@10people.co.uk>
2016-10-20 17:05:05 +0100

    Add lost username endpoint. Fixes #10 (#520)

--- client/forgotPassword/forgotPassword.ctrl.js
+++ client/forgotPassword/forgotPassword.ctrl.js
@@ -6,7 +6,7 @@ module.exports = class ForgotPassword {
     this.$mdToast = $mdToast;
     this.errorHandler = errorHandler;
   }
-  sendRequest () {
+  sendPasswordRequest () {
     return Promise.resolve(this.$http({
       method: 'POST',
       url: `/api/v1/user/${this.resetName}/passwordReset`
@@ -20,6 +20,21 @@ module.exports = class ForgotPassword {
         .hideDelay(4000));
     }).catch({status: 404}, () => this.$scope.forgotForm.user.$setValidity('exists', false))
       .then(() => this.$scope.$apply())
+      .catch(this.errorHandler);
+  }
+  sendUsernameRequest () {
+    return Promise.resolve(this.$http({
+      method: 'POST',
+      url: `/api/v1/user/${window.encodeURIComponent(this.resetEmail)}/forgotUsername`
+    })).then(() => {
+      this.resetEmail = '';
+      this.$scope.forgotForm.$setPristine();
+      this.$scope.forgotForm.$setUntouched();
+      this.$mdToast.show(this.$mdToast.simple()
+        .textContent('If an account exists, we have sent your username; please check your email.')
+        .position('bottom right')
+        .hideDelay(4000));
+    }).then(() => this.$scope.$apply())
       .catch(this.errorHandler);
   }
 };
