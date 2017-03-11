55be4357f2940c7d47412b60b34e9943b9b54535
TSAR <TheSonAlsoRises@users.noreply.github.com>
2017-03-11 20:05:59 +0100

    Initial gen7 documentation (#757)
    * initial gen7 documentation
    
    * update static.module.js
    
    * fix classes
    
    Fix encoding
    
    * Add a redirect from extracting-pk6-files to extracting-pokemon-files
    
    * fix symbols

--- client/static/static.module.js
+++ client/static/static.module.js
@@ -3,16 +3,22 @@ ng.module('porybox.static', ['ngRoute']).config(['$routeProvider', $routeProvide
   [
     'about',
     'donate',
-    'extracting-pk6-files',
+    'extracting-pokemon-files',
     'faq',
     'how-to-pk6-1-bvs',
     'how-to-pk6-2-homebrew',
     'how-to-pk6-3-4-save-files',
     'how-to-pk6-6-decrypted-powersaves',
+    'how-to-pk7-1-bvs',
+    'how-to-pk7-2-homebrew',
+    'how-to-pk7-3-digital-save-files',
+    'how-to-pk7-4-tea',
     'markdown',
     'privacy-policy',
     'tos'
   ].forEach(pageName => {
     $routeProvider.when(`/${pageName}`, {templateUrl: `/static/${pageName}.html`});
   });
+
+  $routeProvider.when('/extracting-pk6-files', {redirectTo: '/extracting-pokemon-files'});
 }]);
