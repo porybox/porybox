f44387cfe23aa14b3ab4020fb11c061cbe2c72dc
Kirzi <blackaurora9@gmail.com>
2017-04-15 15:54:07 -0400

    Update clone hash

--- api/services/PokemonHandler.js
+++ api/services/PokemonHandler.js
@@ -3,21 +3,20 @@ exports.computeCloneHash = pkmn => {
   // Computes a hash of this Pokemon's immutable data, for the purposes of detecting clones.
   // Data that can be changed (e.g. current moves, EVs, evolution state) is not included.
   // If the output of this function changes, a database migration should be run to fix the hashes on everything.
-  const buf = Buffer.alloc(42);
+  const buf = Buffer.alloc(43);
   buf.writeUInt32LE(pkmn.encryptionConstant); // 4 bytes
   buf.write(pkmn.ot, 4, 24, 'utf16le'); // 24 bytes
   buf.writeUInt16LE(pkmn.tid, 28); // 2 bytes
   buf.writeUInt16LE(pkmn.sid, 30); // 2 bytes
   buf.writeUInt32LE(pkmn.pid, 32); // 4 bytes
   buf.writeUInt16LE(pkparse.getPokemonData(pkmn.dexNo).first_evolution_id, 36); // 2 bytes
-  if (exports.isStaticPidEvent(pkmn)) {
-    let compactIvs = 0;
-    [pkmn.ivHp, pkmn.ivAtk, pkmn.ivDef, pkmn.ivSpe, pkmn.ivSpAtk, pkmn.ivSpDef].forEach(value => {
-      compactIvs = compactIvs << 5 | value;
-    });
-    buf.writeUInt32LE(compactIvs >>> 0, 38); // (5 bits per IV) * (6 IVs) + (2 blank bits) = 32 bits = 4 bytes
-    // Total: 42 bytes (initialized in buffer above)
-  }
+  buf.writeUInt8(pkmn.natureId, 38); // 1 byte
+  let compactIvs = 0;
+  [pkmn.ivHp, pkmn.ivAtk, pkmn.ivDef, pkmn.ivSpe, pkmn.ivSpAtk, pkmn.ivSpDef].forEach(value => {
+    compactIvs = compactIvs << 5 | value;
+  });
+  buf.writeUInt32LE(compactIvs >>> 0, 39); // (5 bits per IV) * (6 IVs) + (2 blank bits) = 32 bits = 4 bytes
+    // Total: 43 bytes (initialized in buffer above)
   return require('crypto').createHash('sha256').update(buf).digest('base64');
 };
 
@@ -28,8 +27,6 @@ exports.checkProhibited = pkmn => {
   });
   return matchedProps ? matchedProps.reason : null;
 };
-
-exports.isStaticPidEvent = () => false; // TODO: Implement this
 
 exports.getSafePokemonForUser = async (
     pkmn, user, {checkUnique = false, parse = true, omitBox = false, knownBoxVisibility} = {}
